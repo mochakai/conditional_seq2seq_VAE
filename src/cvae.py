7e2942875511862656a917cd8b3898ded8f17d8f
Ubuntu <ubuntu@ec037-072.maas>
2019-05-08 15:30:14 +0000

    - fix all about CVAE RNN

--- src/cvae.py
+++ src/cvae.py
@@ -48,7 +48,7 @@ LATENT_SIZE = 32
 COND_EMB_SIZE = 8
 #The number of vocabulary
 VOCAB_SIZE = 28
-teacher_forcing_ratio = 0.5
+TEACHER_FORCING_RATIO = 0.5
 empty_input_ratio = 0.1
 KLD_weight = 0.0
 LR = 0.05
@@ -65,29 +65,14 @@ class EncoderRNN(nn.Module):
         self.cond_embedding_size = cond_embedding_size
         self.latent_size = latent_size
 
-        self.embedding_cond = nn.Embedding(cond_size, cond_embedding_size)
         self.embedding_word = nn.Embedding(vocab_size, hidden_size)
         self.gru = nn.GRU(hidden_size, hidden_size)
-        self.linear = nn.Linear(hidden_size, latent_size)
 
-    def forward(self, input, condition, hidden):
-        # h = hidden + condition = HIDDEN_SIZE
-        c = self.embedding_cond(condition).view(1,1,-1)
-        # (1, 1, hidden_size)
-        h = torch.cat((hidden, c), dim=2)
-        embeded = self.embedding_word(input).view(-1, 1, self.hidden_size)       # (word_len, 1, hidden_size)
-        outs, h = self.gru(embeded, h)              # (word_len, 1, hidden_size), (1, 1, hidden_size)
-        
-        # reparameterization trick
-        sample = torch.normal(
-            torch.FloatTensor([0]*self.latent_size), 
-            torch.FloatTensor([1]*self.latent_size)
-        ).to(device)
-        mean = self.linear(h)
-        logvar = self.linear(h)
-        z = sample * torch.exp(logvar/2) + mean
-        
-        return z
+    def forward(self, input, hidden, count=0):
+        embeded = self.embedding_word(input).view(1, 1, -1)
+        outs, h = self.gru(embeded, hidden)              # (word_len, 1, hidden_size), (1, 1, hidden_size)
+
+        return h, outs
 
     def initHidden(self):
         return torch.zeros(1, 1, self.hidden_size - self.cond_embedding_size, device=device)
@@ -100,63 +85,124 @@ class DecoderRNN(nn.Module):
         self.hidden_size = hidden_size
         self.vocab_size = vocab_size
 
-        self.embedding_cond = nn.Embedding(cond_size, cond_embedding_size)
         self.embedding_word = nn.Embedding(vocab_size, hidden_size)
-        self.lat2hid = nn.Linear(latent_size+cond_embedding_size, hidden_size)
         self.gru = nn.GRU(hidden_size, hidden_size)
         self.out = nn.Linear(hidden_size, vocab_size)
 
-    def forward(self, input, latent, condition, teacher_forcing=False):
-        c = self.embedding_cond(condition).view(1,1,-1)
-        # (1,1,latent_size + condition_size)
-        lat = torch.cat((latent, c), dim=2)
-        hidden = self.lat2hid(lat)
-
+    def forward(self, input, hidden, teacher_forcing=False):
         # (word_len, 1, hidden_size)
-        embeded = self.embedding_word(input).view(-1, 1, self.hidden_size)
+        embeded = self.embedding_word(input).view(1, 1, -1)
+        embeded = F.relu(embeded)
         # (word_len, 1, hidden_size), (1, 1, hidden_size)
         outs, h = self.gru(embeded, hidden)
 
         # (word_len, vocab_size)
         outputs = self.out(outs).view(-1, self.vocab_size)
         
-        return outputs
+        return h, outputs
 
 
-def train(input_and_cond, target_and_cond, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion):
-    input_tensor, input_cond = input_and_cond
-    target_tensor, target_cond = target_and_cond
-    sos_tensor = torch.tensor([SOS_token], dtype=torch.long, device=device)
-    eos_tensor = torch.tensor([EOS_token], dtype=torch.long, device=device)
-    
-    encoder_optimizer.zero_grad()
-    decoder_optimizer.zero_grad()
-    loss = 0
+class CVAE(nn.Module):
+    def __init__(self, vocab_size, hidden_size, cond_size, cond_embedding_size, latent_size):
+        super(CVAE, self).__init__()
+        self.latent_size = latent_size
+        
+        self.encoder = EncoderRNN(vocab_size, hidden_size, cond_size, cond_embedding_size, latent_size)
+        self.decoder = DecoderRNN(vocab_size, hidden_size, cond_size, cond_embedding_size, latent_size)
+        self.embedding_cond = nn.Embedding(cond_size, cond_embedding_size)
+        self.lat2hid = nn.Linear(latent_size+cond_embedding_size, hidden_size)
+        self.mean_linear = nn.Linear(hidden_size, latent_size)
+        self.logvar_linear = nn.Linear(hidden_size, latent_size)
 
-    #----------sequence to sequence part for encoder----------#
-    encoder_latent = encoder(input_tensor, input_cond, encoder.initHidden())
-    gt = torch.cat((target_tensor, eos_tensor))
+    def cal_loss(self, input, gt, mean, log_var, kl_weight=0.002):
+        # reconstruction loss
+        # print(input, gt.unsqueeze(0))
+        # RCL = F.cross_entropy(input, gt.unsqueeze(0))
+        # kl divergence loss
+        KLD = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
+        return kl_weight * KLD
+        # return RCL + kl_weight * KLD
 
-    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
-    #----------sequence to sequence part for decoder----------#
-    if use_teacher_forcing:
-        decoder_input = torch.cat((sos_tensor, target_tensor))
-        decoder_output = decoder(decoder_input, encoder_latent, target_cond)
-        loss = criterion(decoder_output, gt)
-    else:
+    def forward(self, input_and_cond, target_and_cond, test=False, show=False):
+        input_tensor, input_cond = input_and_cond
+        target_tensor, target_cond = target_and_cond
+        sos_tensor = torch.tensor([SOS_token], dtype=torch.long, device=device)
+        eos_tensor = torch.tensor([EOS_token], dtype=torch.long, device=device)
+        
+        loss = 0
+        #----------sequence to sequence part for encoder----------#
+        # h = hidden + condition = HIDDEN_SIZE
+        encoder_c = self.embedding_cond(input_cond).view(1,1,-1)
+        # (1, 1, hidden_size)
+        encoder_h = torch.cat((self.encoder.initHidden(), encoder_c), dim=2)
+        for i in range(input_tensor.size(0)):
+            encoder_h, encoder_outs = self.encoder(input_tensor[i], encoder_h)
+
+        # reparameterization trick
+        sample = torch.normal(
+            torch.FloatTensor([0]*self.latent_size), 
+            torch.FloatTensor([1]*self.latent_size)
+        ).to(device)
+        mean = self.mean_linear(encoder_outs)
+        logvar = self.logvar_linear(encoder_outs)
+        encoder_latent = sample.mul(torch.exp(logvar/2)).add_(mean)
+        # z = sample * torch.exp(logvar/2) + mean
+
+        gt = torch.cat((target_tensor, eos_tensor))
+        use_teacher_forcing = True if random.random() < TEACHER_FORCING_RATIO else False
+        #----------sequence to sequence part for decoder----------#
+        decoder_c = self.embedding_cond(target_cond).view(1,1,-1)
+        # (1,1,latent_size + condition_size)
+        lat = torch.cat((encoder_latent, decoder_c), dim=2)
+        hidden = self.lat2hid(lat)
+
+        if test:
+            pred = None
+            decoder_input = sos_tensor
+            decoder_hidden = hidden
+            for i in range(gt.size(0)):
+                decoder_hidden, decoder_output = self.decoder(decoder_input, decoder_hidden, target_cond)
+                decoder_input = decoder_output.max(dim=1)[1]
+                pred = decoder_input if pred is None else torch.cat((pred, decoder_input))
+                if decoder_input.item() == eos_tensor.item():
+                    break
+            pred = result_trans(pred)
+            gt = result_trans(gt)
+            if show:
+                # print('BLEU-4:', compute_bleu(pred, gt))
+                print('{:>20} -> {:20} ans: {}'.format(result_trans(input_tensor)[0], pred[0], gt[0]))
+            return compute_bleu(pred, gt)
+
         pred = None
-        decoder_input = sos_tensor
-        for i in range(gt.size(0)):
-            decoder_output = decoder(decoder_input, encoder_latent, target_cond)
-            decoder_input = decoder_output.max(dim=1)[1]
-            pred = decoder_output if pred is None else torch.cat((pred, decoder_output))
-        loss = criterion(pred, gt)
-
-    loss.backward()
-    encoder_optimizer.step()
-    decoder_optimizer.step()
-
-    return loss
+        loss = 0
+        loss_count = 0
+        if use_teacher_forcing:
+            decoder_input = torch.cat((sos_tensor, target_tensor))
+            for i in range(decoder_input.size(0)):
+                hidden, decoder_output = self.decoder(decoder_input[i], hidden)
+                loss += F.cross_entropy(decoder_output, gt[i].unsqueeze(0))
+                # loss += self.cal_loss(decoder_output, gt[i], mean, logvar)
+                loss_count += 1
+                # print('loss:', loss)
+                # print('='*20)
+                pred = decoder_output if pred is None else torch.cat((pred, decoder_output))
+            # loss = self.cal_loss(pred, gt, mean, logvar)
+            
+        else:
+            decoder_input = sos_tensor
+            for i in range(gt.size(0)):
+                hidden, decoder_output = self.decoder(decoder_input, hidden, target_cond)
+                decoder_input = decoder_output.max(dim=1)[1].detach()
+                loss += F.cross_entropy(decoder_output, gt[i].unsqueeze(0))
+                # loss += self.cal_loss(decoder_output, gt[i], mean, logvar)
+                loss_count += 1
+                # print('loss:', loss)
+                # print('='*20)
+                pred = decoder_output if pred is None else torch.cat((pred, decoder_output))
+            # loss = self.cal_loss(pred, gt, mean, logvar)
+        loss /= loss_count
+        loss += self.cal_loss(pred, gt, mean, logvar)
+        return loss
 
 
 def asMinutes(s):
@@ -173,52 +219,39 @@ def timeSince(since, percent):
     return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
 
 
-def save_checkpoint(encoder, decoder, folder_name, record_dict):
-    torch.save(encoder.state_dict(), os.path.join(folder_name, 'encoder.pkl'))
-    torch.save(decoder.state_dict(), os.path.join(folder_name, 'decoder.pkl'))
-    file_name = os.path.join(folder_name, 'loss_score.json')
-    with open(file_name, 'w') as f:
-        json.dump(record_dict, f)
 
-
-def run_test(encoder, decoder):
-    score = 0
-    test_pairs = get_training_pairs('test.txt', test=True)
-    for test in test_pairs:
-        score += evaluate(tensorsFromPair(test), encoder, decoder, test=True)
-    print('='*70)
-    print('Average BLEU-4:', score / len(test_pairs))
-
-
-def trainIters(encoder, decoder, n_iters, print_every=1000, plot_every=100, learning_rate=0.01):
+def trainIters(cvae, n_iters, print_every=1000, plot_every=100, learning_rate=0.01):
     start = time.time()
     plot_losses = []
     print_loss_total = 0  # Reset every print_every
     plot_loss_total = 0  # Reset every plot_every
 
 
-    encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
-    decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
+    cvae_optimizer = optim.Adam(cvae.parameters())
     pairs = get_training_pairs('train.txt')
     training_pairs = [tensorsFromPair(random.choice(pairs))
                       for i in range(n_iters)]
-    criterion = nn.CrossEntropyLoss()
-
+    
     folder_name = datetime.now().strftime("%m-%d_%H-%M") + '_iter_' + str(n_iters)
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
     full_score_count = 0
-    best_score = 5
+    best_score = 0.5
     record_dict = {'loss': [], 'score': []}
 
     for iter in range(1, n_iters + 1):
         input_and_cond, target_and_cond = training_pairs[iter - 1]
 
-        loss = train(input_and_cond, target_and_cond, encoder, decoder, 
-                     encoder_optimizer, decoder_optimizer, criterion)
+        cvae_optimizer.zero_grad()
+        loss = cvae(input_and_cond, target_and_cond)
+        loss.backward()
+        cvae_optimizer.step()
+
+        # loss /= (target_and_cond[0].size(0) + 1)
         print_loss_total += loss
         record_dict['loss'].append(loss.item())
-        record_dict['score'].append(evaluate(training_pairs[iter - 1], encoder, decoder, show=False))
+        # record_dict['score'].append(evaluate(training_pairs[iter - 1], encoder, decoder, show=False))
+        
 
         if iter % print_every == 0:
             print_loss_avg = print_loss_total / print_every
@@ -226,18 +259,39 @@ def trainIters(encoder, decoder, n_iters, print_every=1000, plot_every=100, lear
             print('='*70)
             print('%s (%d %d%%) %.4f' % (timeSince(start, iter / n_iters),
                                          iter, iter / n_iters * 100, print_loss_avg))
-            score = evaluate(tensorsFromPair(random.choice(pairs)), encoder, decoder)
-            
-            save_checkpoint(encoder, decoder, folder_name, record_dict)
-            full_score_count = full_score_count+1 if score >= 1 else 0
-            if full_score_count > best_score:
-                best_score = full_score_count
-                new_folder = os.path.join(folder_name, 'score_' + str(full_score_count) + '_iter_' + str(iter))
+            score = eval(input_and_cond, target_and_cond, cvae, show=True)
+            # score = evaluate(tensorsFromPair(random.choice(pairs)), encoder, decoder)
+
+            avg_test_score = run_test(cvae)
+            save_checkpoint(cvae, folder_name, record_dict)
+            if avg_test_score > best_score:
+                best_score = avg_test_score
+                new_folder = os.path.join(folder_name, 'score_' + int(avg_test_score * 100) + '_iter_' + str(iter))
                 if not os.path.exists(new_folder):
                     os.makedirs(new_folder)
-                save_checkpoint(encoder, decoder, new_folder, record_dict)
+                save_checkpoint(cvae, folder_name, record_dict)
+        
+        if iter % plot_every == 0:
+            record_dict['score'].append(run_test(cvae, False))
 
-    run_test(encoder, decoder)
+
+def save_checkpoint(cvae, folder_name, record_dict):
+    torch.save(cvae.state_dict(), os.path.join(folder_name, 'cvae.pkl'))
+    file_name = os.path.join(folder_name, 'loss_score.json')
+    with open(file_name, 'w') as f:
+        json.dump(record_dict, f)
+
+
+def run_test(cvae, show=True):
+    score = 0
+    test_pairs = get_training_pairs('test.txt', test=True)
+    for test in test_pairs:
+        input_and_cond, target_and_cond = tensorsFromPair(test)
+        score += eval(input_and_cond, target_and_cond, cvae, show=show)
+    if show:
+        print('Average BLEU-4:', score / len(test_pairs))
+        print('='*70)
+    return score / len(test_pairs)
 	
 
 def get_training_pairs(input_file, test=False):
@@ -246,9 +300,7 @@ def get_training_pairs(input_file, test=False):
         all_words = np.loadtxt(input_file, dtype=np.str)
         for four_tense_word in all_words:
             for i in range(4):
-                for j in range(4):
-                    if i != j:
-                        res.append([[four_tense_word[i], i], [four_tense_word[j], j]])
+                res.append([[four_tense_word[i], i], [four_tense_word[i], i]])
     else:
         all_words = np.loadtxt(input_file, dtype=np.str)
         label = make_test_labels()
@@ -299,6 +351,13 @@ def compute_bleu(output, reference):
     return sentence_bleu([reference], output,weights=(0.25, 0.25, 0.25, 0.25),smoothing_function=cc.method1)
 
 
+def eval(input_and_cond, target_and_cond, cvae, show=False):
+    cvae.eval()
+    score = cvae(input_and_cond, target_and_cond, test=True, show=show)
+    cvae.train()
+    return score
+
+
 def evaluate(pair, encoder, decoder, test=False, show=True):
     input_and_cond, target_and_cond = pair
     with torch.no_grad():
@@ -306,28 +365,43 @@ def evaluate(pair, encoder, decoder, test=False, show=True):
         target_tensor, target_cond = target_and_cond
         sos_tensor = torch.tensor([SOS_token], dtype=torch.long, device=device)
         eos_tensor = torch.tensor([EOS_token], dtype=torch.long, device=device)
+        
+        for i in range(input_tensor.size(0)):
+            encoder_latent, z_mu, z_var = encoder(input_tensor[i], input_cond, encoder.initHidden())
+        gt = torch.cat((target_tensor, eos_tensor))
 
-        encoder_latent = encoder(input_tensor, input_cond, encoder.initHidden())
         decoder_input = torch.cat((sos_tensor, target_tensor))
+        teacher_pred = None
+        for i in range(decoder_input.size(0)):
+            decoder_output = decoder(decoder_input, encoder_latent, target_cond)
+        teacher_pred = result_trans(decoder_output.max(dim=1)[1])
 
-        decoder_output = decoder(decoder_input, encoder_latent, target_cond)
-        pred = result_trans(decoder_output.max(dim=1)[1])
+        total_output = None
+        decoder_input = sos_tensor
+        for _ in range(gt.size(0)):
+        # while decoder_input.item() != eos_tensor.item():
+            decoder_output = decoder(decoder_input, encoder_latent, target_cond)
+            decoder_input = decoder_output.max(dim=1)[1]
+            total_output = decoder_input if total_output is None else torch.cat((total_output, decoder_input))
+            # if decoder_input.item() == eos_tensor.item():
+            #     break
+        pred = result_trans(total_output)
         gt = result_trans(target_tensor)
 
         if show:
             if not test:
                 print('BLEU-4:', compute_bleu(pred, gt))
             print('{:>20} -> {:20} ans: {}'.format(result_trans(input_tensor)[0], pred[0], gt[0]))
+            print('teacher: ', teacher_pred)
             # print('pred:', pred)
             # print('true:', gt)
 
         return compute_bleu(pred, gt)
 
 
-def load_checkpoint(encoder, decoder, args):
-    encoder.load_state_dict(torch.load(os.path.join(args.load, 'encoder.pkl')))
-    decoder.load_state_dict(torch.load(os.path.join(args.load, 'decoder.pkl')))
-    run_test(encoder, decoder)
+def load_checkpoint(cvae, args):
+    cvae.load_state_dict(torch.load(os.path.join(args.load, 'cvae.pkl')))
+    run_test(cvae)
 
 
 def get_args():
@@ -339,12 +413,11 @@ def get_args():
 
 if __name__ == "__main__":
     args = get_args()
-    encoder_init = EncoderRNN(VOCAB_SIZE, HIDDEN_SIZE, len(TENSE), COND_EMB_SIZE, LATENT_SIZE).to(device)
-    decoder_init = DecoderRNN(VOCAB_SIZE, HIDDEN_SIZE, len(TENSE), COND_EMB_SIZE, LATENT_SIZE).to(device)
+    cvae_init = CVAE(VOCAB_SIZE, HIDDEN_SIZE, len(TENSE), COND_EMB_SIZE, LATENT_SIZE).to(device)
     if args.load:
-        load_checkpoint(encoder_init, decoder_init, args)
+        load_checkpoint(cvae_init, args)
     else:
-        trainIters(encoder_init, decoder_init, 200000, print_every=1000)
+        trainIters(cvae_init, 200000, print_every=1000)
 
     sys.stdout.flush()
     sys.exit()
